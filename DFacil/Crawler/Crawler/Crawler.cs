0b6a85d4269cfaa65968302a765cd957080af9d6
abelardo <abbbekgb@gmail.com>
2012-08-07 12:45:34 -0700

    adicionado passo para ler padroes

---
+++ DFacil/Crawler/Crawler/Crawler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Crawler
+{
+    class Crawler
+    {
+
+        public static MatchCollection Execute(string filepath, string pattern)
+        {
+            string content = File.ReadAllText(filepath);
+            Regex c = new Regex(pattern, RegexOptions.Compiled | RegexOptions.Multiline);
+            MatchCollection f_result_collection = c.Matches(content);
+            var len = f_result_collection.Count;
+            Console.WriteLine(String.Format("Total encontrado {0}", len.ToString()));
+            if (len > 0)
+            {
+                Console.WriteLine(String.Format("Primeiro match encontrado \n\n{0}\n", f_result_collection[0].Groups["padrao"]));
+                Console.WriteLine(String.Format("Segundo match encontrado \n\n{0}\n", f_result_collection[1].Groups["padrao"]));
+            }
+            return f_result_collection;
+        }
+
+        public static void MatchAndSave(string file_to_read, string file_to_write, string pattern)
+        {
+            string content = File.ReadAllText(file_to_read);
+            Regex c = new Regex(pattern, RegexOptions.Compiled | RegexOptions.Multiline);
+            MatchCollection match_collection = c.Matches(content);
+            List<String> matches = new List<String>();
+            foreach (Match m in match_collection)
+            {
+                matches.Add(m.Groups["padrao"].ToString());
+            }
+            File.WriteAllLines(file_to_write, matches.ToArray());
+        }
+    }
+}
---
+++ DFacil/Crawler/Crawler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\Sevana\Desktop\DiarioFacil\python\889.txt";
+            string pattern = @"(?<padrao>^[ ]*Nº \d{7}-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4} - Apelação (?:\s|.)*?)\r\r\n\r\r\n";
+            string filepath_write = @"C:\Users\Sevana\Desktop\DiarioFacil\python\889_m.txt";
+
+            //MatchCollection m = Crawler.Execute(filepath, pattern);
+            Crawler.MatchAndSave(filepath, filepath_write, pattern);
+        }
+    }
+}
