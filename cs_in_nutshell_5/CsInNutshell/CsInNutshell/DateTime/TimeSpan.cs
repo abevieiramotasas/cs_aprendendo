645e20ccc248d915165e949473a7e08bdcaac018
Abelardo Vieira Mota <abbbekgb@gmail.com>
2012-08-19 17:09:21 -0300

    Atualizando

---
+++ cs_in_nutshell_5/CsInNutshell/CsInNutshell/DateTime/TimeSpan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Nutshell.DateTimet
+{
+    [TestFixture]
+    class TimeSpanTeste
+    {
+        [Test]
+        [TestCase(2.5, 30, Result = 3)]
+        public double SumHourMinutesTest(Double a, Double b)
+        {
+            TimeSpan t = TimeSpan.FromHours(a) + TimeSpan.FromMinutes(b);
+            return t.Hours;
+        }
+
+        [Test]
+        public void DiaTem24HorasTest()
+        {
+            Assert.AreEqual(TimeSpan.FromDays(1.0), TimeSpan.FromHours(24));
+        }
+
+        [Test]
+        public void DiferençaDeTotalTest()
+        {
+            Assert.AreNotEqual(TimeSpan.FromHours(2.5).Hours, TimeSpan.FromHours(2.5).TotalHours);
+        }
+    }
+}
---
+++ cs_in_nutshell_5/CsInNutshell/CsInNutshell/Encoding/EncodingTeste.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Nutshell.Encoding
+{
+    [TestFixture]
+    class EncodingTeste
+    {
+        [Test]
+        public void IANAEncodingNamesTest()
+        {
+            foreach (EncodingInfo ef in System.Text.Encoding.GetEncodings())
+            {
+                Console.WriteLine(ef.Name);
+            }
+        }
+        
+        [Test]
+        public void ConvertBytesTest(
+            [Values("Abelardo", "Bilila heh")] string name,
+            [Values("GB18030", "utf-8")] string encoding_name)
+        {
+            System.Text.Encoding e = System.Text.Encoding.GetEncoding(encoding_name);
+            byte[] byte_name = e.GetBytes(name);
+            Assert.AreEqual(name, e.GetString(byte_name));
+        }
+
+        // to vendo que entendo pn de encoding
+        [Test]
+        public void Convert2EncodingTypeTest(
+            [Values("Hehe", "Oi tudo bem", "Abelardo Vieira Mota")] string msg)
+        {
+            System.Text.Encoding[] es = new System.Text.Encoding[2];
+            es[0] = System.Text.Encoding.GetEncoding("GB18030");
+            es[1] = System.Text.Encoding.GetEncoding("utf-8");
+            Assert.AreEqual(es[0].GetBytes(msg), es[1].GetBytes(msg));
+        }
+
+    }
+}
